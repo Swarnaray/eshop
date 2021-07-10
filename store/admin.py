c83b08b34e81d11ba75afa4d243431f1eeb65dbc
Swarnaray <Swarna94.official@gmail.com>
2021-07-11 00:21:27 +0530

    admin panel updated

--- store/admin.py
+++ store/admin.py
@@ -8,14 +8,17 @@ from .models.orders import Order
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name', 'price', 'category']
 
-
-
 class AdminCategory(admin.ModelAdmin):
     list_display = ['name']
 
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['customer', 'product', 'quantity', 'price', 'date', 'status']    
 
 # Register your models here.
 admin.site.register(Product, AdminProduct)
 admin.site.register(Category , AdminCategory)
-admin.site.register(Customer )
-admin.site.register(Order )
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Order, AdminOrder)
--- store/models/product.py
+++ store/models/product.py
@@ -8,6 +8,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=200, default='' , null=True , blank=True)
     image = models.ImageField(upload_to='uploads/products/')
+    
+    def __str__(self):
+        return self.name
 
     @staticmethod
     def get_products_by_id(ids):
