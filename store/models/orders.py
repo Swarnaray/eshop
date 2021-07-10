b6327123a3cbe8cf98b1af2f9253fdce3c1b8894
Swarnaray <Swarna94.official@gmail.com>
2021-07-11 03:20:06 +0530

    templates Updated

--- store/models/orders.py
+++ store/models/orders.py
@@ -5,16 +5,23 @@ import datetime
 
 
 class Order(models.Model):
-    product = models.ForeignKey(Product,
-                                on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer,
-                                 on_delete=models.CASCADE)
+    STATUS =(
+        ('Pending','Pending'),
+        ('Order Confirmed','Order Confirmed'),
+        ('Shipped','Shipped'),
+        ('Out for Delivery','Out for Delivery'),
+        ('Delivered','Delivered'),
+        ('Canceled','Canceled')
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     price = models.IntegerField()
     address = models.CharField(max_length=50, default='', blank=True)
     phone = models.CharField(max_length=50, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
-    status = models.BooleanField(default=False)
+    status=models.CharField(max_length=50,null=True,choices=STATUS)
+    # status = models.BooleanField(default=False)
 
     def placeOrder(self):
         self.save()
